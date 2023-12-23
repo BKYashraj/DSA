661b5a8cb1a1d9e277da45f0bfefcbaf527cd4c1
Yashraj Pravin Desale <119423317+BKYashraj@users.noreply.github.com>
2023-12-23 08:42:02 +0530

    Create 59. Spiral Matrix II.cpp

---
+++ 2D Array/59. Spiral Matrix II.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int minr = 0, minc = 0;
+        int maxr = n-1, maxc = n-1;
+        vector<vector<int>>matrix(n,vector<int>(n));
+        int value = 1;
+        while(minr<=maxr && minc<=maxc){
+            for(int j = minc;j<=maxc;j++){
+                matrix[minr][j] = value++;
+            }
+            minr++;
+            if(minr>maxr || minc>maxc) break;
+            for(int i=minr;i<=maxr;i++){
+                matrix[i][maxc] = value++;
+            }
+            maxc--;
+            if(minr>maxr || minc>maxc) break;
+            for(int j=maxc;j>=minc;j--){
+                matrix[maxr][j] = value++;
+            }
+            maxr--;
+            if(minr>maxr || minc>maxc) break;
+            for(int i=maxr;i>=minr;i--){
+                matrix[i][minc] = value++;
+            }
+            minc++;
+        }
+        return matrix;
+    }
+};
