589d7538d5894ad19781e401b2e056f4593a0a6f
Yashraj Pravin Desale <119423317+BKYashraj@users.noreply.github.com>
2024-02-25 21:41:48 +0530

    53. Maximum Subarray.cpp

---
+++ Striver’s SDE Sheet 191/53. Maximum Subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int maxi = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            if(sum > maxi) maxi = sum;
+            if(sum < 0) sum = 0;
+        }
+        return maxi;
+    }
+};
