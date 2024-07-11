1503969b088ab6909b264f752a8cd8953fa72207
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-07-11 12:19:11 +0530

    101. Symmetric Tree.cpp

---
+++ Tree/101. Symmetric Tree.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool helper(TreeNode* lefts, TreeNode* rights){
+        if(lefts == NULL && rights == NULL) return true;
+        if(lefts == NULL || rights == NULL) return false;
+        if(lefts->val != rights->val) return false;
+        return helper(lefts->left,rights->right) && helper(lefts->right,rights->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+        TreeNode* lefts = root->left;
+        TreeNode* rights = root->right;
+        return helper(lefts,rights);
+    }
+};
+
+-----------------OR------------------
+
+class Solution {
+public:
+    bool helper(TreeNode* p, TreeNode* q){
+        if(!p && !q) return true;
+        if(!p || !q) return false;
+        if(p->val == q->val && helper(p->left,q->right) && helper(p->right,q->left)) return true;
+        else return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL) return true;
+        return helper(root->left,root->right);
+    }
+};
