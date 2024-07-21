eadb5934fa1d246bbf6771b1f51913f76276abf7
Yashraj Pravin Desale <yashrajdesale1@gmail.com>
2024-07-21 18:41:16 +0530

    3226. Number of Bit Changes to Make Two Integers Equal.cpp

---
+++ Bit Manipulation/3226. Number of Bit Changes to Make Two Integers Equal.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minChanges(int n, int k) {
+        int ans = 0;
+        for(int i=0;i<32;i++){
+            int a = (n >> i & 1);
+            int b = (k >> i & 1);
+            if(a == 0 && b == 1) return -1;
+            if(a == 1 && b == 0) ans++;
+        }
+        return ans;
+    }
+};
